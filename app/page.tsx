9ed111e9828eca2481556b9bc6a856298975c656
Prasad Pathare <prasadsachinist10@gmail.com>
2025-01-09 07:03:41 +0000

    add BookAdd component with orange theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+"use client";
+
+import BookAdd from "@/components/BookAdd";
+import BookList from "@/components/BookList";
 import Header from "@/components/Header";
 
 function Page() {
   return (
     <main>
       <Header />
+      <BookAdd />
+      <BookList />
     </main>
   );
 }
